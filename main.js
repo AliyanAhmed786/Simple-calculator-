1f948b7ecda985370fdb3f66013c5143d1920c8a
Aliyan <e.aliyan2001@gmail.com>
2024-04-03 00:07:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+//my code run command npx esrun main.ts//
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number",
+        type: "number",
+        name: "firstName", },
+    {
+        message: "Enter second number",
+        type: "number",
+        name: "secondName",
+    },
+    {
+        message: "Select the operation type",
+        choices: ['Addition', "Subtract", "Multiplication", "Division",],
+        name: "operator",
+        type: "list"
+    },
+]);
+console.log(answer);
+//conditional statement
+if (answer.operator == 'Addition') {
+    console.log(answer.firstName + answer.secondName);
+}
+else if (answer.operator == 'Subtract') {
+    console.log(answer.firstName - answer.secondName);
+}
+else if (answer.operator == 'Division') {
+    console.log(answer.firstName / answer.secondName);
+}
+else if (answer.operator == 'Multiplication') {
+    console.log(answer.firstName * answer.secondName);
+}
+else
+    ("invalid operator");
